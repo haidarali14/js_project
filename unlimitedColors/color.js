c647f33fa3fc8998222b1ea5b8d7e4558a60299e
haidarali14 <haidaralimarediya@gmail.com>
2024-06-06 15:55:14 +0530

    third commit

---
+++ unlimitedColors/color.js
@@ -0,0 +1,23 @@
+const randomColor = function(){
+    const hex = '0123456789ABCDEF';
+    let color = '#'
+    for(let i = 0; i<6; i++){
+      color += hex[Math.floor(Math.random()*16)]
+    }
+    return color
+  }
+  let interval;
+  const startChangingColor = function(){
+  if(!interval){
+    interval= setInterval(changecolor,1000)
+  }
+   function changecolor(){
+     document.body.style.backgroundColor = randomColor();
+   }
+  };
+  const stopChangingColor = function(){
+    clearInterval(interval);
+    interval = null;
+  };
+  document.querySelector('#start').addEventListener('click',startChangingColor)
+  document.querySelector('#stop').addEventListener('click',stopChangingColor)
